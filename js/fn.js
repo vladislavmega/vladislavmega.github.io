d38f9edca78c912eae7fb3086a1f5597a8547212
vladislavmega <var@intellisoftware.net>
2013-11-06 04:32:22 +0200

    forgotten sites, remove shadows

--- js/fn.js
+++ js/fn.js
@@ -16,7 +16,7 @@ $(document).ready(function(){
 	
 	$('#start_layer .grid > div > a.f').click(function(){
 		createFrame($(this).attr('href'));
-		$('div#show_site .description').html($(this).attr('rel'));
+
 		$('.url').html('<a href="'+$(this).attr('href')+'" target="blank">'+$(this).attr('href')+'</a>');
 		return false;
 	});
@@ -31,7 +31,7 @@ $(document).ready(function(){
 });
 
 function createFrame(site) {
-   c1 = '<iframe src="'+site+'" width="'+$(document).width()*0.9+'" height="'+$(document).height()*0.8+'" scrolling="yes"></iframe>';
+   c1 = '<iframe src="'+site+'" width="100%" height="100%" scrolling="yes"></iframe>';
    console.log(c1);
    $('div#show_site #framewrapper').html(c1);
    $('div#show_site').fadeIn("slow");
@@ -40,5 +40,5 @@ function createFrame(site) {
 function normalizeSize(){
 //    $('#start_layer .grid.first').css({'margin-left': ($(document).width()-957)/2});
     $('#show_site').css({'margin-left' : ($(document).width()*0.1)/2+'px'});
-    $('#start_layer .logo').css({'margin-top':($(document).height()-480)/2+'px'})
+    $('#start_layer .logo').css({'margin-top':($(document).height()-500)/2+'px'})
 }
